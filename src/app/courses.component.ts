58b65ad57a334c9464317ac1dc0bb54bc7d72543
Stephen Parsons <zup_dudes@yahoo.com>
2018-08-24 16:52:32 -0400

    Add Directive *ngFor - loop over courses array

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // <courses>
-    template: '<h2>{{ "Title:" + getTitle() }}</h2>'
+    template: `
+        <h2>List of Courses</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
-    title = "List of Courses"
-
-    getTitle(){
-        return this.title;
-    }
+    title = "List of courses";
+    courses = ["course1", "course2","course3"];
 }
