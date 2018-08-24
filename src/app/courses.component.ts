719391662b5f63c13e18d287c79fe1c65b908d7f
Stephen Parsons <zup_dudes@yahoo.com>
2018-08-24 16:41:20 -0400

    String Interpolation suing a method or property

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // <courses>
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ "Title:" + getTitle() }}</h2>'
 })
 export class CoursesComponent {
+    title = "List of Courses"
 
+    getTitle(){
+        return this.title;
+    }
 }
