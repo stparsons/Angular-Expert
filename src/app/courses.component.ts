7e6280c636ed1f1695352ffcc4ee284614d5201d
Stephen Parsons <zup_dudes@yahoo.com>
2018-08-28 08:23:03 -0400

    Style Binding -- HTML Dynamic

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', // <courses>
     template: `
-    <button class="btn btn-primary" [class.active] = "isActive">Save</button>
+    <button class="btn btn-primary" [style.backgroundColor] = "isActive ? 'blue' : 'white'" >Save</button>
     `
 })
 export class CoursesComponent {
