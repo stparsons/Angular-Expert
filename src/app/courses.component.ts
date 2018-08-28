45a673088ffaba9484af9b40f8410ad9993fc0be
Stephen Parsons <zup_dudes@yahoo.com>
2018-08-28 08:19:58 -0400

    Class Binding -- HTML Dynamic

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,12 +4,13 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', // <courses>
     template: `
-    <button class="btn btn-primary">Save</button>
+    <button class="btn btn-primary" [class.active] = "isActive">Save</button>
     `
 })
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    isActive = false;
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
