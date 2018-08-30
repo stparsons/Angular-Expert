63c43be9139b8361258b2fde4378e2a83c8d6424
Stephen Parsons <zup_dudes@yahoo.com>
2018-08-30 11:31:33 -0400

    Style : Moved style to favorite.component.css

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -3,14 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css'],
-  styles: [
-    `
-      .glyphicon {
-          color:green;
-      }
-    `
-  ]
+  styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent {
   @Input('isFavorite') isSelected: boolean;
