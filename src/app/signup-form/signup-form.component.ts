44783b0b60bb233932ba063702e815dfda890752
Stephen Parsons <zup_dudes@yahoo.com>
2018-10-17 15:46:37 -0400

    Nested FormGroups

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -9,14 +9,16 @@ import { UsernameValidators } from './username.validators';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3), 
-      UsernameValidators.cannotContainSpace
-    ],
-    UsernameValidators.shouldBeUnique
-    ),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+        username: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3), 
+          UsernameValidators.cannotContainSpace
+        ],
+        UsernameValidators.shouldBeUnique
+        ),
+        password: new FormControl('', Validators.required)
+    })
   });
 
   login() {
@@ -26,6 +28,6 @@ export class SignupFormComponent {
   }
  
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
